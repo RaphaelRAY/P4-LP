9f358210fe85b10333ba76d69e4ca945e46b02e7
Raphael Fernandes Raymundo <100390575+RaphaelRAY@users.noreply.github.com>
2023-11-28 09:36:52 -0300

    Comecando a codigo

--- P4/p4/src/main/java/main/Main.java
+++ P4/p4/src/main/java/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        JOptionPane.showMessageDialog(null, "Hello world!");
     }
 }
